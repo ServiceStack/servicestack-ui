14cdf7ae610999b0ae718d3e77017e968fab3dc1
Demis Bellot <demis.bellot@gmail.com>
2022-03-29 12:47:18 +0800

    move .d.ts to top-level

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,26 @@
 // Type definitions for ServiceStack UI
 // Project: https://servicestack.net
 
-import ui = require('./lib/ui');
-export = ui;
+/*
+import * as admin from './admin'
+import * as client from './client'
+import * as explorer from './explorer'
+import * as locode from './locode'
+import * as shared from './shared'
+
+declare module "ui/admin" {
+    export = admin;
+}
+declare module "ui/client" {
+    export = client;
+}
+declare module "ui/explorer" {
+    export = explorer;
+}
+declare module "ui/locode" {
+    export = locode;
+}
+declare module "ui/shared" {
+    export = shared;
+}
+*/
